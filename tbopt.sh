0af5d2da6c13aa7227076cc8e0221c7992560789
mverissi <marcos_verissimo@id.uff.br>
2017-02-07 15:36:06 -0200

    Added line with environment variable for libraries - now user has to change it only once

--- tbopt.sh
+++ tbopt.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+export LAPACK_LIBS="-L/usr/lib -llapack -lblas"
 cd ./src
 gfortran  -c -O2  -ffast-math  sto_ov.f90
 gfortran  -c -O2  -ffast-math  supercell.f90
 gfortran  -c -O2  -ffast-math  postproc.f90 
-gfortran  -c -O2  -ffast-math  diagonalize.f90 -L/usr/lib -llapack  -lblas
-gfortran  -c -O2  -ffast-math  toten.f90  -L/usr/lib -llapack  -lblas
-gfortran  -c -O3  -ffast-math  Huckel_TB.f90  -L/usr/lib -llapack -lblas
-gfortran  -o  htb.x  Huckel_TB.o  sto_ov.o  supercell.o  postproc.o  diagonalize.o toten.o  -L/usr/lib -llapack -lblas
+gfortran  -c -O2  -ffast-math  diagonalize.f90 ${LAPACK_LIBS}
+gfortran  -c -O2  -ffast-math  toten.f90 ${LAPACK_LIBS}
+gfortran  -c -O3  -ffast-math  Huckel_TB.f90 ${LAPACK_LIBS}
+gfortran  -o  htb.x  Huckel_TB.o  sto_ov.o  supercell.o  postproc.o  diagonalize.o toten.o ${LAPACK_LIBS}
 rm *.o *.mod
 cd ..
 mv ./src/htb.x .
